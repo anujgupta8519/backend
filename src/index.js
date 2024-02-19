e18c58200c5bb027f8bc94d17e3a059262ab721a
Anuj Gupta <129574489+anujgupta8519@users.noreply.github.com>
2024-02-19 12:14:20 +0530

    ApiError,ApiResponse,asynceHandler are Created and cros, body-parse and cookie-parser are done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is startd on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Mongo db Connection faild !!",error);
+})
 
 
 
